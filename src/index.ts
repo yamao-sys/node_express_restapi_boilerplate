19aa6612169b1dc023e6f78b9cf7b25e6dd89fe4
yamao-sys <yamaopanku@gmail.com>
2024-01-21 12:02:59 +0900

    TODOリストのCRUD APIを作成

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path'
+import bodyParser from 'body-parser'
 import { AppDataSource } from './data-source';
+
+import Todo from './entities/Todo';
 
 const app = express()
 
@@ -9,6 +12,8 @@ app.use(express.urlencoded({
 	extended: true
 }))
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(bodyParser.json())
 
 app.post('/api/v1/quiz', function (req, res) {
 	const answer = req.body.answer
@@ -25,6 +30,94 @@ app.get('/', function (req, res) {
 
 app.get('/about', function (req, res) {
   res.send('about page')
+})
+
+app.get('/todos', async function (req, res) {
+	const todos = await AppDataSource.getRepository(Todo).find();
+	res.json({
+		result: "SUCCESS",
+		data: todos
+	});
+})
+
+app.get('/todos/:id', async function (req, res) {
+	const todo = await AppDataSource.getRepository(Todo).findOne({
+		where: {
+			id: Number(req.params.id)
+		}
+	});
+	res.json({
+		result: "SUCCESS",
+		data: todo
+	});
+})
+
+app.get('/todos/:id', async function (req, res) {
+	const todo = await AppDataSource.getRepository(Todo).findOne({
+		where: {
+			id: Number(req.params.id)
+		}
+	});
+	res.json({
+		result: "SUCCESS",
+		data: todo
+	});
+})
+
+app.post('/todos', async function (req, res) {
+	const todo = new Todo();
+
+	console.log(req.body);
+	console.log(req.body.title);
+	console.log(req.body.content);
+
+	todo.title = req.body.title;
+	todo.content = req.body.content;
+	await AppDataSource.manager.save(todo)
+	res.json({
+		result: "SUCCESS",
+		data: todo
+	});
+})
+
+app.put('/todos/:id', async function (req, res) {
+	const todoRepository = AppDataSource.getRepository(Todo);
+
+	const todo = await todoRepository.findOneBy({
+		id: Number(req.params.id)
+	});
+
+	if (!todo) {
+		return res.status(500).send("このTODOは存在しません");
+	}
+
+	todo.title = req.body.title;
+	todo.content = req.body.content;
+
+	await todoRepository.save(todo);
+
+	res.json({
+		result: "SUCCESS",
+		data: todo
+	});
+})
+
+app.delete('/todos/:id', async function (req, res) {
+	const todoRepository = AppDataSource.getRepository(Todo);
+
+	const todo = await todoRepository.findOneBy({
+		id: Number(req.params.id)
+	});
+
+	if (!todo) {
+		return res.status(500).send("このTODOは存在しません");
+	}
+
+	await todoRepository.remove(todo);
+
+	res.json({
+		result: "SUCCESS"
+	});
 })
 
 app.listen(1234, async function() {
